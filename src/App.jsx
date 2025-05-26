f8bf2e850fbc0eefe936c896fd7a2c35d4284a2f
Umberto Amenta <umberto.amenta@hotmail.it>
2025-05-26 17:50:54 +0200

    feat: milestone 1 (mostrare la lista dei prodotti)

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
-export default function App() {}
+export default function App() {
+  const products = [
+    { name: "Mela", price: 0.5 },
+    { name: "Pane", price: 1.2 },
+    { name: "Latte", price: 1.0 },
+    { name: "Pasta", price: 0.7 },
+  ];
+
+  return (
+    <div className="list">
+      {products.map((product) => (
+        <div className="product">
+          <span>{product.name}</span>
+          <span>{product.price}€</span>
+        </div>
+      ))}
+    </div>
+  );
+}
